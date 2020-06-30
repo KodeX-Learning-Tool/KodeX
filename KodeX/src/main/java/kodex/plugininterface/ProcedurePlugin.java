2c541992b3685feb4034d0b8d2470444c4ee66fa
Patrick <uvwut@student.edu>
2020-06-30 16:32:35 +0200

    implemented ProcedurePlugin

--- KodeX/src/main/java/kodex/plugininterface/ProcedurePlugin.java
+++ KodeX/src/main/java/kodex/plugininterface/ProcedurePlugin.java
@@ -1,38 +1,56 @@
 package kodex.plugininterface;
 
 /**
+ * Diese Klasse bildet die Kommunikationsschnittstelle zwischen einem Plugin und dem
+ * Framework. Alle Klassen, die diese Klasse erweitern, werden vom PluginLoader mit Hilfe
+ * des ServiceLoaders geladen und als Verfahrensplugin angesehen. Somit kommuniziert das
+ * Framework initial mit einer generalisierung dieser Klasse.
  * 
+ * @author Patrick Spiesberger
+ * 
+ * @version 1.0
+ *
  */
 public abstract class ProcedurePlugin implements Pluginable {
 
     /**
-     * Default constructor
+     * Konstruktor der Klasse ProcedurePlugin
      */
-    public ProcedurePlugin() {
-    }
+    public ProcedurePlugin() {}
 
     /**
-     * @return
+     * Liefert die Startstufe des Verfahrens zurück, wobei die Stufen 
+     * als doppelt verkettete Liste intern gespeichert werden und 
+     * dementsprechend miteinander verbunden sind
+     * @return Presenter für die erste Stufe der Verfahrens
      */
     public abstract ChainLinkPresenter getChainHead();
 
     /**
-     * @param content
+     * Setzt beim kodieren den Content für die erste Stufe des Verfahrens.
+     * Zudem erstellt die Methode alle Stufen des Verfahrens und
+     * initialisiert diese somit
+     * @param content : Content für die erste Stufe des Verfahrens
      */
     public abstract void initEncodeProcedure(Content content);
 
     /**
-     * @param content
+     * Setzt beim dekodieren den Content für die erste Stufe des Verfahrens.
+     * Zudem erstellt die Methode alle Stufen des Verfahrens und
+     * initialisiert diese somit
+     * @param content : Content für die erste Stufe des Verfahrens
      */
     public abstract void initDecodeProcedure(Content content);
 
     /**
-     * @return
+     * Erzeugt eine neue ProcedureInformation Instanz und gibt diese zurück
+     * @return Erzeugte Instanz von ProcedureInformation
      */
     public abstract ProcedureInformation createProcedureInformation();
 
     /**
-     * @return
+     * Erzeugt eine neue ImportPresenter Instanz und gibt diese zurück
+     * @return Erzeugte Instanz von ImportPresenter
      */
     public abstract ImportPresenter createImportPresenter();
 
