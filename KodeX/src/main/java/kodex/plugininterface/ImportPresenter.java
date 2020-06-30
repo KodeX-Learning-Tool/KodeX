4213b7f4be85167acc7a9f233e70edcd84b73bc6
Patrick <uvwut@student.edu>
2020-06-30 16:49:31 +0200

    implemented ImportPresenter | TODO: setLayoutPresenter

--- KodeX/src/main/java/kodex/plugininterface/ImportPresenter.java
+++ KodeX/src/main/java/kodex/plugininterface/ImportPresenter.java
@@ -5,58 +5,73 @@ import kodex.presenter.IPresenter;
 import kodex.presenter.ProcedureLayoutPresenter;
 
 /**
+ * Diese Klasse ist für die Eingabe eines Verfahrens zuständig. Sie verwaltet die 
+ * entsprechende View mit welchem die Eingabe für das Verfahren erfolgt und mit 
+ * welchem zwischen kodieren und dekodieren unterschieden wird. 
+ * Zudem enthält sie die Funktionalität, mit welcher die Eingabe erfolgt 
+ * (z.B. Datei importieren oder Text eingeben). Des Weitern ist sie dafür
+ * zuständig die Eingabe zu verifizieren, aus ihr den Content für die
+ * Verfahrenseingabe zu erstellen und diesen an das Verfahren zu übergebenü
+ * und es somit zu initialisieren. Ein Importer für ein Verfahren muss 
+ * diese Klasse erweitern.
  * 
+ * @author Patrick Spiesberger
+ * 
+ * @version 1.0
+ *
  */
 public abstract class ImportPresenter implements IPresenter {
+	
+	/* Konkretes Plugin, für welches der Import Presenter erstellt wird */
+	protected ProcedurePlugin plugin;
 
     /**
-     * Default constructor
+     * Konstruktor, in welchem das ProcedurePlugin übergeben wird ,um in
+     * diesem das Verfahren initialisieren zu können wenn eine erfolgreiche
+     * Eingabe passiert ist.
+     * @param plugin : Plugin, welches diesen Importer benutzt
      */
-    public ImportPresenter() {
+    public ImportPresenter(ProcedurePlugin plugin) {
+        this.plugin = plugin;
     }
 
-
-
+    
     /**
-     * @return
+     * Validiert die Eingabe eines Verfahrens welche kodiert werden soll
+     * @return true, falls Eingagbe valide zum kodieren ist, ansonsten false
      */
     public abstract boolean validateEncodeImport();
 
     /**
-     * @return
+     * Validiert die Eingabe eines Verfahrens welche dekodiert werden soll
+     * @return true, falls Eingagbe valide zum dekodieren ist, ansonsten false
      */
     public abstract boolean validateDecodeImport();
 
     /**
-     * 
+     * Wird aufgerufen, falls Eingabe zum kodieren geladen wird und behandelt diese
      */
     public abstract void handleEncodeImport();
 
     /**
-     * 
+     * Wird aufgerufen, falls Eingabe zum dekodieren geladen wird und behandelt diese
      */
     public abstract void handleDecodeImport();
 
     /**
-     * @param plugin
-     */
-    public void ImportPresenter(ProcedurePlugin plugin) {
-        // TODO implement here
-    }
-
-    /**
-     * @param plp
+     * Setzt den LayoutPresenter für diesen Import, um auf diesem einen 
+     * Wechsel zum ChainPresenter einzuleiten, wenn die Eingabe erfolgreich war
+     * @param plp : zu setzender LayoutPresenter
      */
     public void setLayoutPresenter(ProcedureLayoutPresenter plp) {
         // TODO implement here
     }
 
     /**
-     * @return
+     * Gibt die View zum Importieren des Contents zurück
+     * @return View in Form eines AnchorPane, in dem der Content importiert
+     * 		   werden kann
      */
-    public AnchorPane getView() {
-        // TODO implement here
-        return null;
-    }
+    public abstract AnchorPane getView();
 
 }
