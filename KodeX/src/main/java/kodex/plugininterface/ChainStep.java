a4d657cd91325d447f59c3c06c46e3291ea21bb1
Patrick <uvwut@student.edu>
2020-06-30 16:22:48 +0200

    implemented ChainStep

--- KodeX/src/main/java/kodex/plugininterface/ChainStep.java
+++ KodeX/src/main/java/kodex/plugininterface/ChainStep.java
@@ -1,27 +1,33 @@
 package kodex.plugininterface;
 
-import java.util.*;
-
 /**
+ * Diese Klasse stellt einen abstrakten Schritt zwischen zwei Stufen dar.
+ * Sie beinhaltet die Funktionalität um den Content zwischen zwei explizit
+ * festgelegten Stufen dekodieren bzw.kodieren zu können.
+ * Konkrete Schritte müssen diese Klasse erweitern.
  * 
+ * @author Patrick Spiesberger
+ * 
+ * @version 1.0
+ *
  */
 public abstract class ChainStep {
 
     /**
-     * Default constructor
+     * Konstruktor der Klasse ChainStep
      */
-    public ChainStep() {
-    }
+    public ChainStep() {}
 
     /**
-     * @param left 
-     * @param right
+     * Kodiert den Content von einer Stufe und setzt ihn in den Content ihrer nächsten Stufe
+     * @param left : der Content der Stufe der kodiert werden soll
+     * @param right : der Content der Stufe welcher gesetzt werden soll
      */
     public abstract void encode(Content left, Content right);
 
     /**
-     * @param right 
-     * @param left
+     * @param right : der Content der Stufe der dekodiert werden soll
+     * @param left : der Content der Stufe welcher gesetzt werden soll
      */
     public abstract void decode(Content right, Content left);
 
