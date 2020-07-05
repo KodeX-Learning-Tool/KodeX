bf41934a7a732ff4415a410c6a72aee36ebb9026
Leo Kraft <kraftleonhard@gmx.de>
2020-07-05 17:35:19 +0200

    Style(PressenterManager): Change formatting.

--- KodeX/src/main/java/kodex/presenter/PresenterManager.java
+++ KodeX/src/main/java/kodex/presenter/PresenterManager.java
@@ -7,8 +7,9 @@ import kodex.model.SideMenuTypes;
 
 /**
  * 
- * This class manages the main presenters and keeps a reference to the root stage.
- * It gets the views for the root stage from its current presenter and side menu presenter.
+ * This class manages the main presenters and keeps a reference to the root
+ * stage. It gets the views for the root stage from its current presenter and
+ * side menu presenter.
  * 
  * @author Leonhard Kraft
  * @author Yannick Neubert
@@ -36,61 +37,63 @@ public class PresenterManager {
      * The layout for the scene contained in the root stage.
      */
     private BorderPane rootLayout;
-    
+
     /**
      * Creates a new PresenterManaegr with a reference to the root stage.
      * 
      * @param rootStage The root stage of the application.
      */
     public PresenterManager(Stage rootStage) {
-        
+
         this.rootStage = rootStage;
-        
-        //BorderPane is used, because it allowes to set the center (main page) and left (side menu)
+
+        // BorderPane is used, because it allowes to set the center (main page) and left
+        // (side menu)
         this.rootLayout = new BorderPane();
-        
-        //create Scene with a BorderPane Layout for the root stage
+
+        // create Scene with a BorderPane Layout for the root stage
         Scene rootScene = new Scene(rootLayout);
         rootStage.setScene(rootScene);
     }
 
     /**
-     * This method initiates the updating of the main view by setting the center of the BorderPane layout
-     * with the view of the current presenter.
+     * This method initiates the updating of the main view by setting the center of
+     * the BorderPane layout with the view of the current presenter.
      */
     private void updateMainView() {
-        
+
         this.rootLayout.setCenter(this.currentPresenter.getView());
     }
-    
+
     /**
-     * This method initiates the updating of the side menu view by setting the left side of the BorderPane layout
-     * with the view of the SideMenuPresenter.
+     * This method initiates the updating of the side menu view by setting the left
+     * side of the BorderPane layout with the view of the SideMenuPresenter.
      */
     private void updateSideMenuView() {
-        
+
         this.rootLayout.setLeft(this.sideMenuPresenter.getView());
     }
 
     /**
-     * This method sets the desired side menu type in the SideMenuPresenter and 
+     * This method sets the desired side menu type in the SideMenuPresenter and
      * refreshes the displayed view.
      * 
      * @param type The desired side menu type.
      */
     public void updateSideMenuView(SideMenuTypes type) {
-        
+
         this.sideMenuPresenter.changeSideMenuType(type);
         updateSideMenuView();
     }
 
     /**
-     * This method sets the presenter that is currently active and displays its view.
+     * This method sets the presenter that is currently active and displays its
+     * view.
      * 
      * @param newPresenter The main presenter that should display its view next.
      */
     public void updatePresenter(Presenter newPresenter) {
-        
+
         this.currentPresenter = newPresenter;
         updateMainView();
     }
@@ -101,7 +104,7 @@ public class PresenterManager {
      * @param sideMenuPresenter The SideMenuPresenter for the displayed side menu.
      */
     public void setSideMenuPresenter(SideMenuPresenter sideMenuPresenter) {
-        
+
         this.sideMenuPresenter = sideMenuPresenter;
         updateSideMenuView();
     }
