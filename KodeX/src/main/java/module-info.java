7e3645e3858bb898c33aad3afb9f93c67ad93388
uzidy <uzidy@student.kit.edu>
2020-07-05 20:36:55 +0200

    feat(module-info): add auto-generated requirement
    - automatically added javafx.base requirement

--- KodeX/src/main/java/module-info.java
+++ KodeX/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module kodex.KodeX {
     requires javafx.controls;
     requires javafx.fxml;
 	requires javafx.graphics;
+	requires javafx.base;
 
     opens kodex.presenter to javafx.fxml;
     exports kodex;
