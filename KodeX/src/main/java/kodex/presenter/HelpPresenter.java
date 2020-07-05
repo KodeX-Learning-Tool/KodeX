3b7b5043dc5c5b2cf151efe2bf8bcf89b81d08d2
uzidy <uzidy@student.kit.edu>
2020-07-05 21:21:53 +0200

    feat(HelpPresenter): add javafx related code
    - added an initialize method
    
    - added a helper method to create FAQ items
    
    - added necessary imports

--- KodeX/src/main/java/kodex/presenter/HelpPresenter.java
+++ KodeX/src/main/java/kodex/presenter/HelpPresenter.java
@@ -1,6 +1,17 @@
 package kodex.presenter;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import kodex.model.Help;
 
 /**
@@ -14,6 +25,23 @@ import kodex.model.Help;
  */
 public class HelpPresenter extends Presenter {
 	
+	@FXML
+	private VBox faqBox;
+	
+	@FXML
+	private TextFlow infoBox;
+	
+	@FXML
+	private Text infoText;
+	
+	private ArrayList<TitledPane> faq = new ArrayList<>();
+	
+	private List<String> questions = new ArrayList<>();
+	
+	private List<String> answers = new ArrayList<>();
+	
+	private String text;
+		
     /**
      * Die Instanz der Hilfe Klasse mit dem FAQ
      */
@@ -28,5 +56,43 @@ public class HelpPresenter extends Presenter {
     	
     	help = new Help();
     }
+    
+	@FXML
+	private void initialize() {	
+		questions = help.getQuestions();
+		answers = help.getAnswers();
+		
+		Iterator<String> qIt = questions.iterator();
+		
+		Iterator<String> aIt = answers.iterator();
+		
+		while (qIt.hasNext() && aIt.hasNext()) {
+			String question = qIt.next();
+			String answer = aIt.next();
+			
+			faq.add(createFAQElement(question, answer));
+		}
+		
+		
+		for (String info : help.getInfo()) {
+			text = text.concat(info + "\n");
+		}
+		
+		infoText.setText(text);
+		
+		for (TitledPane element: faq) {
+			faqBox.getChildren().add(element);
+			faqBox.getChildren().add(new Separator());
+		}
+	}
+	
+	private TitledPane createFAQElement(String question, String answer) {
+		TextFlow answerBlock = new TextFlow(new Text(answer));
+		answerBlock.getStyleClass().add("help-page__faq__element__content");
+		answerBlock.setPadding(new Insets(0,0,0,50));
+		TitledPane element = new TitledPane("Q: " + question, answerBlock);
+		element.getStyleClass().add("help-page__faq__element");
+		element.setExpanded(false);
+		return element;
 	}
 }
