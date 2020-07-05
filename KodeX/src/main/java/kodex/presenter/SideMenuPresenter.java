e8a4bb6d6469d407cbf0de6f226606f9d583ca78
Leo Kraft <kraftleonhard@gmx.de>
2020-07-05 18:21:09 +0200

    implement(SideMenuPresenter)

--- KodeX/src/main/java/kodex/presenter/SideMenuPresenter.java
+++ KodeX/src/main/java/kodex/presenter/SideMenuPresenter.java
@@ -1,82 +1,83 @@
 package kodex.presenter;
 
-import javafx.scene.layout.AnchorPane;
 import kodex.model.SideMenuTypes;
 
 /**
- * Dieser Presenter ist für das Seitenmenü zuständig. 
- * Durch Klicken der Schaltflächen des Menüs kann der Benutzer 
- * schnell zu anderen Haupt-Presentern wechseln.
+ * This presenter is responsible for the side menu. By clicking on the
+ * navigation items the user can switch between different main presenter
+ * classes.
  * 
+ * @author Leonhard Kraft
  * @author Yannick Neubert
  * 
  * @version 1.0
  */
 public class SideMenuPresenter extends Presenter {
 
+    private PresenterFactory presenterFactroy;
+
     /**
-     * Erstellt einen neuen SideMenuPresenter mit Referenzen zu dem PresenterManager 
-     * und einer PresenterFactory.
-     * @param pm : Die Referenz zum PresenterManger, um zu anderen Fenstern wechseln zu können.
-     * @param pf : Die Referenz zur PresenterFactory mit welcher der Presenter andere Presenter erstellen kann.
+     * Creates a new SideMenuPresenter with a reference to the PresenterManager and
+     * a PresenterFactory.
+     * 
+     * @param presenterManager The reference to the PresenterManager to switch
+     *                         between different main presenter classes.
+     * @param presenterFactroy The reference to a PresenterFactory which is used to
+     *                         create new presenters.
      */
-    public SideMenuPresenter(PresenterManager pm, PresenterFactory pf) {
-        // TODO implement here
+    public SideMenuPresenter(PresenterManager presenterManager, PresenterFactory presenterFactroy) {
+        super(presenterManager, "sidemenu");
+        this.presenterFactroy = presenterFactroy;
     }
 
     /**
-     * Die Methode ändert den Typ des Seitenmenüs. Hierbei kann man zwischen dem standardmäßigen
-     * oder dem minimierten Seitenmenü wählen.
-     * @param type : Der Typ des Seitenmenüs.
+     * This method changes the type of the side menu. It can be switched between a
+     * standard and a minimized side menu.
+     * The type decides what FXML file should be loaded.
+     * 
+     * @param type The type of side menu that should be loaded.
      */
     public void changeSideMenuType(SideMenuTypes type) {
         // TODO implement here
     }
 
     /**
-     * Die Methode wird ausgeführt, wenn der Benutzer auf die Startfenster-Schaltfläche klickt. 
-     * Wechselt zum IndexPagePresenter und benachrichtigt den PresenterManger.
+     * This method is executed when the user clicks on the index page button.
+     * It initiates the change to a IndexPagePresenter.
      */
     public void handleIndexPage() {
-        // TODO implement here
+        this.presenterManager.updatePresenter(this.presenterFactroy.createIndexPagePresenter());
     }
 
     /**
-     * Die Methode wird ausgeführt, wenn der Benutzer auf die Netzwerkfenster-Schaltfläche klickt. 
-     * Wechselt zum NetworkPresenter und benachrichtigt den PresenterManger.
+     * This method is executed when the user clicks on the network button.
+     * It initiates the change to a NetworkPresenter.
      */
     public void handleNetwork() {
-        // TODO implement here
+        this.presenterManager.updatePresenter(this.presenterFactroy.createNetworkPresenter());
     }
 
     /**
-     * Die Methode wird ausgeführt, wenn der Benutzer auf die Einstellungen-Schaltfläche klickt. 
-     * Wechselt zum SettingsPresenter und benachrichtigt den PresenterManger.
+     * This method is executed when the user clicks on the settings button.
+     * It initiates the change to a SettingsPresenter.
      */
     public void handleSettings() {
-        // TODO implement here
+        this.presenterManager.updatePresenter(this.presenterFactroy.createSettingsPresenter());
     }
 
     /**
-     * Die Methode wird ausgeführt, wenn der Benutzer auf die Plugins-Schaltfläche klickt. 
-     * Wechselt zum PluginsMenuPresenter und benachrichtigt den PresenterManger.
+     * This method is executed when the user clicks on the plugins button.
+     * It initiates the change to a PluginPresenter.
      */
     public void handlePlugins() {
-        // TODO implement here
+        this.presenterManager.updatePresenter(this.presenterFactroy.createPluginMenuPresenter());
     }
 
     /**
-     * Die Methode wird ausgeführt, wenn der Benutzer auf die Hilfe-Schaltfläche klickt. 
-     * Wechselt zum HelpPresenter und benachrichtigt den PresenterManger.
+     * This method is executed when the user clicks on the help button.
+     * It initiates the change to a HelpPresenter.
      */
     public void handleHelp() {
-        // TODO implement here
+        this.presenterManager.updatePresenter(this.presenterFactroy.createHelpPresenter());
     }
-
-	@Override
-	public AnchorPane getView() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
