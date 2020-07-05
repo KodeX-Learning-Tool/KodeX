1bfa0f34c939387ec3d053d21393d1f1c42308d8
uzidy <uzidy@student.kit.edu>
2020-07-05 20:30:21 +0200

    feat(PluginMenuPresenter): add javafx related code
    - added various necessary imports
    
    - added initialize-method
    
    - added implementation for methods handleAddPlugin & handleRemovePlugin
    
    - added @FXML tag to handleRefreshPlugins

--- KodeX/src/main/java/kodex/presenter/PluginMenuPresenter.java
+++ KodeX/src/main/java/kodex/presenter/PluginMenuPresenter.java
@@ -1,7 +1,17 @@
 package kodex.presenter;
 
+import java.io.File;
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import kodex.model.PluginLoader;
+import kodex.plugininterface.Pluginable;
 
 /**
  * Dieser Presenter ist für die Pluginmenüseite zuständig. Auf dieser Seite werden die Plugins 
@@ -13,6 +23,18 @@ import kodex.model.PluginLoader;
  * @version 1.0
  */
 public class PluginMenuPresenter extends Presenter {
+	
+	@FXML
+	TableView<Pluginable> pluginTable;
+	
+	@FXML
+	TableColumn<Pluginable, Boolean> checkBoxColumn;
+	
+	@FXML 
+	TableColumn<Pluginable, String> nameColumn;
+	
+	@FXML 
+	TableColumn<Pluginable, String> descriptionColumn;
 	
     /**
      * Die Instanz des PluginLoaders mit seiner liste an Plugins
@@ -30,27 +52,65 @@ public class PluginMenuPresenter extends Presenter {
         
         pluginLoader = PluginLoader.getInstance();
     }
+    
+	@FXML
+	private void initialize() {		
+		/*
+		 * checkBoxColumn.setCellValueFactory(c -> {
+		 * c.getValue().activatedProperty().addListener((observer, oldValue, newValue)
+		 * -> { if (newValue.booleanValue()) {
+		 * pluginLoader.activatePlugin(c.getValue()); } else {
+		 * pluginLoader.deactivatePlugin(c.getValue()); } }); return
+		 * c.getValue().activatedProperty(); });
+		 * checkBoxColumn.setCellFactory(CheckBoxTableCell.forTableColumn(checkBoxColumn
+		 * ));
+		 * 
+		 * nameColumn.setCellValueFactory(cellData ->
+		 * cellData.getValue().nameProperty());
+		 * descriptionColumn.setCellValueFactory(cellData ->
+		 * cellData.getValue().shortDescriptionProperty());
+		 * 
+		 * pluginTable.setItems(pluginLoader.getEnabledPlugins());
+		 * pluginTable.setEditable(true);
+		 */
+	}
 
     /**
      * Die Methode wird ausgeführt, wenn der Benutzer auf die Schaltfläche zum Hinzufügen 
      * eines Plugins klickt. Öffnet den System-Explorer zum Auswählen des Plugins und lädt diesen falls möglich.
      */
-    public void handleAddPlugin() {
-        // TODO implement here
-    }
+	@FXML
+	private void handleAddPlugin() throws IOException {
+		FileChooser chooser = new FileChooser();
+		chooser.setTitle("Plugin zum importieren auswählen.");
+		chooser.getExtensionFilters().add(new ExtensionFilter("JAR-Datei", "*.jar"));
+		File file = chooser.showOpenDialog(null);
+		if (file != null) {
+			pluginLoader.addPlugin(file);	
+		} else {
+			System.out.println("No plugin chosen.");
+		}
+	}
 
     /**
      * Die Methode wird ausgeführt, wenn der Benutzer auf die Schaltfläche zum Entfernen 
      * eines Plugins klickt. Löscht das Plugin aus dem Plugins-Ordner.
      */
-    public void handleRemovePlugin() {
-        // TODO implement here
-    }
+	@FXML
+	private void handleRemovePlugin() throws IOException {
+		Pluginable plugin = pluginTable.getSelectionModel().getSelectedItem();
+		if (plugin != null) {
+			pluginLoader.removePlugin(plugin);	
+		} else {
+			System.out.println("No plugin selected.");
+		}
+	}
 
     /**
      * Die Methode wird ausgeführt, wenn der Benutzer auf die Schaltfläche zum Aktualisieren 
      * eines Plugins klickt. Lädt alle Plugins im Plugin-Ordner.
      */
+	@FXML
     public void handleRefreshPlugins() {
         // TODO implement here
     }
