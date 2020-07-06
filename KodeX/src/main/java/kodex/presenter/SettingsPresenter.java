736f38e3a8282bb884e1f9775f9560794ba82a26
Leo Kraft <kraftleonhard@gmx.de>
2020-07-06 12:23:09 +0200

    feat(SettingsPresenter): Implement basic validation for the port textfield.

--- KodeX/src/main/java/kodex/presenter/SettingsPresenter.java
+++ KodeX/src/main/java/kodex/presenter/SettingsPresenter.java
@@ -1,5 +1,11 @@
 package kodex.presenter;
 
+import java.io.IOException;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import kodex.model.DefaultSettings;
 
 /**
@@ -19,6 +25,14 @@ public class SettingsPresenter extends Presenter {
      */
     private DefaultSettings defaultSettings;
 
+    @FXML
+    private TextField portTextField;
+
+    /*
+     * Indicates whether the content of the port TextField is valid.
+     */
+    private boolean validPortTextField;
+
     /**
      * Creates a new SettingsPresenter with a reference to the PresenterManager
      * 
@@ -31,8 +45,29 @@ public class SettingsPresenter extends Presenter {
     }
 
     /**
+     * Initializes the view-object created by the FXMLLoader.
+     * 
+     * @throws IOException is thrown when the fxml file couldn't get loaded
+     *                     properly.
+     */
+    @FXML
+    private void initialize() throws IOException {
+
+        /*
+         * Initialize the port setting.
+         */
+        portTextField.textProperty().addListener(createPortChangeListener());
+
+        String portString = Integer.toString(defaultSettings.getPort());
+        portTextField.setText(portString);
+
+        validPortTextField = true;
+    }
+
+    /**
      * This Method is called when a new Language is selected. Changes the language.
      */
+    @FXML
     public void handleChangeLanguage() {
         // TODO implement here
     }
@@ -41,6 +76,7 @@ public class SettingsPresenter extends Presenter {
      * This Method is called when the Dark-Mode has been de-/activated. Changes the
      * appearance of the application by changing the loaded css file.
      */
+    @FXML
     public void handleChangeSkin() {
         // TODO implement here
     }
@@ -49,30 +85,95 @@ public class SettingsPresenter extends Presenter {
      * This Method is called when the user clicks on the item to change the default
      * path for saving files. Opens a dialog to change the path and saves it.
      */
+    @FXML
     public void handleBrowsePath() {
-        // TODO implement here
+        // TODO implement with FileChooser
+    }
+
+    private ChangeListener<String> createPortChangeListener() {
+
+        /*
+         * Use listener to replace invalid text with the old value. This is not possible
+         * if you use a normal handle method because the old value is not known.
+         */
+        return new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+
+                validatePort(oldValue, newValue);
+            }
+        };
     }
 
     /**
      * This Method is called when the user enters text into the text field for the
-     * default port.
+     * default port and the text has to be validated.
      */
-    public void handlePortText() {
-        // TODO implement here
+    private void validatePort(String oldValue, String newValue) {
+        /*
+         * Only ports in range [0, 65535] are allowed for the ServerSocket. A port
+         * number of 0 means that the port number is automatically allocated. Source:
+         * https://docs.oracle.com/javase/7/docs/api/java/net/ServerSocket.html
+         */
+
+        /*
+         * Validate Textfield content: 
+         * -text consists only of numerical digits 
+         * -text length is in the range [0, 5] 
+         * -text as integer is in the accepted port range [0, 65535]
+         */
+
+        /*
+         * Only 5 digits are allowed and since the port is an integer only numerical
+         * digits are accepted
+         */
+        if (!newValue.matches("\\d{0,5}")) {
+            portTextField.setText(oldValue);
+            return;
+        }
+
+        if (newValue.isEmpty()) {
+            // TextField is empty
+            // TODO what to show here?
+            return;
+        }
+
+        int portNumber = Integer.parseInt(newValue);
+
+        if ((portNumber < 0) || (65535 < portNumber)) {
+            // not in the given port range
+
+            // TODO indicate wrong port visually
+            validPortTextField = false;
+            return;
+        }
+
+        // all checks have passed so the new content of the text field is valid
+        validPortTextField = true;
     }
 
     /**
      * This Method is called when the user clicks on the item to confirm the entered
      * default port. Saves the entered port.
      */
+    @FXML
     public void handleSubmitPort() {
-        // TODO implement here
+
+        if (!validPortTextField) {
+            // TODO visually highlight, that the textfield is not valid yet
+            return;
+        }
+
+        int portNumber = Integer.parseInt(portTextField.getText());
+
+        defaultSettings.setPort(portNumber);
     }
 
     /**
      * This Method is called when the user clicks on the item to restore the default
      * settings. Resets the settings.
      */
+    @FXML
     public void handleRestoreDefaultSettings() {
         // TODO implement here
     }
