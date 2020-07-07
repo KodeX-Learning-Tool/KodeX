f967d3805e29545413ab4672f89406c564e33864
uzidy <uzidy@student.kit.edu>
2020-07-07 20:24:36 +0200

    docs(ChainPresenter): add JavaDoc-comments
    - added new or edited existing JavaDoc-comments
    
    - added additional author tag

--- KodeX/src/main/java/kodex/presenter/ChainPresenter.java
+++ KodeX/src/main/java/kodex/presenter/ChainPresenter.java
@@ -21,38 +21,35 @@ import kodex.plugininterface.ProcedurePlugin;
  * the view for a given Procedure-Plugin, hiding a Chain Link, and jumping to a Chain Link.
  * 
  * @author Yannick Neubert
+ * @author Raimon Gramlich
  * 
  * @version 1.0
  */
 public class ChainPresenter implements IPresenter {
-
-    /**
-     * The scrollable Pane in which the split pane is nested.
-     */
-    /**
-     * The split pane in which the Chain Links are displayed.
-     */
-    
-
-    /**
-     * Creates a new ChainPresenter.
-     */
 	
+    /** The scrollable Pane in which the split pane is nested. */
 	@FXML
 	private ScrollPane viewScrollPane;
 	
+    /** The split pane in which the Chain Links are displayed. */
 	@FXML
 	private SplitPane chainSplitPane;
 	
+	/** The view object containing everything which is displayed in the chain. */
 	private AnchorPane chainRootPane;
 	
+	/** The reference to the first ChainLinkPresenter. */
 	private ChainLinkPresenter firstChainLinkPresenter;
 	
+	/** The reference to the ProcedureLayoutPresenter. */
 	private ProcedureLayoutPresenter procedureLayoutPresenter;
 	
     /**
-     * This method is executed when the users clicks on the button to hide a Chain Link. It minimizes 
-     * the Chain Link in the Split-Pane.
+     * Creates a new ChainPresenter with a reference to the first ChainLinkPresenter
+     * and a ProcedureLayoutPresenter.
+     * 
+     * @param chainLinkPresenter : The reference to the first ChainLinkPresenter.
+     * @param procedureLayoutPresenter : The reference to a ProcedureLayoutPresenter.
      */
     public ChainPresenter(ChainLinkPresenter chainLinkPresenter, ProcedureLayoutPresenter procedureLayoutPresenter) {
     	this.firstChainLinkPresenter = chainLinkPresenter;
@@ -68,28 +65,50 @@ public class ChainPresenter implements IPresenter {
 		}
     }
 	
+	/**
+	 * This class represents a single chain link. It loads a template via fxml
+	 * file and fills it with the content it has gotten from an instance of the 
+	 * ChainLinkPresenter class.
+	 * 
+	 * @author Raimon Gramlich
+	 * 
+	 * @version 1.0
+	 */
     private class ChainItem extends AnchorPane {
 		
+    	/** The Label for displaying the name of the chain link. */
 		@FXML
 		private Label titleLabel;
 		
+		/** The BorderPane to the ProcedureLayoutPresenter. */
 		@FXML
 		private BorderPane chainLinkPane;
 		
+		/** The reference to the ProcedureLayoutPresenter. */
 		@FXML
 		private VBox informationBox;
 		
+		/** The reference to the ProcedureLayoutPresenter. */
 		@FXML
 		private FontIcon hideButtonIcon;
 		
+		/** The reference to the ProcedureLayoutPresenter. */
 		private Boolean isHidden;
 		
+		/** The reference to the ProcedureLayoutPresenter. */
 		private String hiddenIcon = "mdi-chevron-down";
 		
+		/** The reference to the ProcedureLayoutPresenter. */
 		private String shownIcon = "mdi-chevron-right";
 		
+		/** The reference to the ProcedureLayoutPresenter. */
 		private ChainLinkPresenter chainLinkPresenter;
 		
+	    /**
+	     * Creates a new ChainItem with a reference to its ChainLinkPresenter.
+	     * 
+	     * @param chainLinkPresenter : The reference to the ChainLinkPresenter.
+	     */
 		ChainItem(ChainLinkPresenter chainLinkPresenter) {
 			this.chainLinkPresenter = chainLinkPresenter;
 			
@@ -106,17 +125,29 @@ public class ChainPresenter implements IPresenter {
 			
 		}
 		
+	    /**
+	     * Initializes the view-object created by the FXMLLoader.
+	     * Sets the chain item name and adds the header view object to the displayed view.
+	     */
 		@FXML
 		private void initialize() {
 			// titleLabel.setText("Kodierungsstufe: " + chainLinkPresenter.getName());
 			informationBox.getChildren().set(0, chainLinkPresenter.getChainLinkHeaderView());
 		}
 		
+	    /**
+	     * This method is executed when the users clicks on the button to edit a Chain Link.
+	     * It opens the editor window.
+	     */
 		@FXML
 		private void handleEdit() {
 			procedureLayoutPresenter.setEditPresenter(chainLinkPresenter.getChainLinkEditPresenter());
 		}
 		
+	    /**
+	     * This method is executed when the users clicks on the button to export the content of a chain link.
+	     * It writes the content to the disk in a file specified by the user.
+	     */
 		@FXML
 		private void handleExport() {
 			FileChooser chooser = new FileChooser();
@@ -126,11 +157,16 @@ public class ChainPresenter implements IPresenter {
 			chainLinkPresenter.export(exportLocation);
 		}
 		
+	    /**
+	     * This method is executed when the users clicks on the button to hide a Chain Link. It minimizes 
+	     * the Chain Link in the Split-Pane.
+	     */
 		@FXML
 		private void handleHideChainLink() {
 			toggleHide();
 		}
 		
+	    /** Toggles between the flag isHidden between true and false, while changing the icon accordingly.  */
 		private void toggleHide() {
 			if (isHidden) {
 				hideButtonIcon.setIconLiteral(shownIcon);
@@ -144,6 +180,7 @@ public class ChainPresenter implements IPresenter {
     
     /**
      * This method creates the view for a given Procedure-Plugin.
+     * 
      * @param activeProcedure the active Procedure-Plugin.
      */
     public void createChainView(ProcedurePlugin activeProcedure) {		
@@ -160,6 +197,7 @@ public class ChainPresenter implements IPresenter {
 
     /**
      * This method jumps to the Chain Link with the given ID.
+     * 
      * @param id : The ID of the Chain Link.
      */
     public void jumpToChainLink(int id) {
