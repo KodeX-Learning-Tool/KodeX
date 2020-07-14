6bba7609636e3de96a06757d546a3adcd9653d75
uzidy <uzidy@student.kit.edu>
2020-07-14 19:10:43 +0200

    refactor(PluginMenuPresenter): add I18N support
    - adjusted pluginpage.fxml
    
    - added textProperty bindings in PluginMenuPresenter
    
    - added necessary Button fields
    
    - added translation entries in property files

--- KodeX/src/main/java/kodex/presenter/PluginMenuPresenter.java
+++ KodeX/src/main/java/kodex/presenter/PluginMenuPresenter.java
@@ -4,11 +4,13 @@ import java.io.File;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import kodex.model.I18N;
 import kodex.model.PluginLoader;
 import kodex.plugininterface.Pluginable;
 
@@ -40,6 +42,14 @@ public class PluginMenuPresenter extends Presenter {
 	@FXML 
 	TableColumn<Pluginable, String> descriptionColumn;
 	
+	/** The Button which allows the user to import new plugins.*/
+	@FXML
+	Button addPluginButton;
+	
+	/** The Button which allows the user to remove currently loaded plugins.*/
+	@FXML
+	Button removePluginButton;
+	
     /** The instance of the PluginLoader class which you can use to work with the loaded plugins. */
     private PluginLoader pluginLoader;
 
@@ -60,7 +70,14 @@ public class PluginMenuPresenter extends Presenter {
      * It fills the table with data gathered from the PluginLoader.
      */
 	@FXML
-	private void initialize() {		
+	private void initialize() {
+		// bind the text properties to the language file values
+		checkBoxColumn.textProperty().bind(I18N.createStringBinding("pluginpage.activeColumn.header"));
+		nameColumn.textProperty().bind(I18N.createStringBinding("pluginpage.nameColumn.header"));
+		descriptionColumn.textProperty().bind(I18N.createStringBinding("pluginpage.descriptionColumn.header"));
+		addPluginButton.textProperty().bind(I18N.createStringBinding("pluginpage.addbutton"));
+		removePluginButton.textProperty().bind(I18N.createStringBinding("pluginpage.removebutton"));
+		
 		/* // defines the check box column
 		 * checkBoxColumn.setCellValueFactory(c -> {
 		 * c.getValue().activatedProperty().addListener((observer, oldValue, newValue)
