e6479a10935183d1ee8d7d69595227cd0cefd1dd
uzidy <uzidy@student.kit.edu>
2020-07-18 16:54:25 +0200

    feat(ColorImageImportPresenter): implent simple

--- KodeX/src/main/java/kodex/standardplugins/colorimageprocedure/presenter/ColorImageImportPresenter.java
+++ KodeX/src/main/java/kodex/standardplugins/colorimageprocedure/presenter/ColorImageImportPresenter.java
@@ -1,6 +1,15 @@
 package kodex.standardplugins.colorimageprocedure.presenter;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import kodex.plugininterface.ChainLinkPresenter;
 import kodex.plugininterface.ImportPresenter;
 import kodex.plugininterface.ProcedurePlugin;
 
@@ -9,6 +18,8 @@ import kodex.plugininterface.ProcedurePlugin;
  */
 public class ColorImageImportPresenter extends ImportPresenter {
 
+	AnchorPane importview; 
+
 	public ColorImageImportPresenter(ProcedurePlugin plugin) {
 		super(plugin);
 		// TODO Auto-generated constructor stub
@@ -16,34 +27,59 @@ public class ColorImageImportPresenter extends ImportPresenter {
 
 	@Override
 	public boolean validateEncodeImport() {
-		// TODO Auto-generated method stub
-		return false;
+		return plugin.getChainHead().getContent().isValid(input);
 	}
 
 	@Override
 	public boolean validateDecodeImport() {
-		// TODO Auto-generated method stub
-		return false;
+		ChainLinkPresenter clp = plugin.getChainHead();
+		
+		while (clp.getNext() != null) {
+			clp = clp.getNext();
+		}	
+		
+		return clp.getContent().isValid(input);;
 	}
 
 	@Override
 	public void handleEncodeImport() {
-		// TODO Auto-generated method stub
+		File file = importFile("Kodieren");
 		
+		Image image = new Image(file.getPath());
+		
+		validateEncodeImport();
 	}
 
 	@Override
 	public void handleDecodeImport() {
-		// TODO Auto-generated method stub
+		File file = importFile("Dekodieren");
 		
+		String binaryString = new Scanner(file).next();
+		
+		validateDecodeImport();
 	}
 
 	@Override
 	public AnchorPane getView() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// loads the template
+		try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("importexample.fxml"));
+            loader.setController(this);
+            importview = loader.load();
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+		
+		return importview;
 	}
-
+	
+	
+	private File importFile(String msg) {		
+		FileChooser fc = new FileChooser();
+		fc.setTitle("Datei zum " + msg + " auswählen.");
+		return fc.showOpenDialog(null);
+	}
 
 
 }
