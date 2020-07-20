2bbc569c83c623cf3ff9179d5791777bbc4e7d0f
Leo Kraft <kraftleonhard@gmx.de>
2020-07-20 19:32:50 +0200

    feat(RLE-ImportPresenter) implement ImportPresenter for RLE

--- KodeX/src/main/java/kodex/standardplugins/rle/presenter/RLEImportPresenter.java
+++ KodeX/src/main/java/kodex/standardplugins/rle/presenter/RLEImportPresenter.java
@@ -1,49 +1,127 @@
 package kodex.standardplugins.rle.presenter;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import kodex.plugininterface.ChainLinkPresenter;
 import kodex.plugininterface.ImportPresenter;
 import kodex.plugininterface.ProcedurePlugin;
+import kodex.pluginutils.model.content.LetterString;
+import kodex.pluginutils.model.content.TupleString;
 
 /**
  * 
  */
 public class RLEImportPresenter extends ImportPresenter {
+    
+    private String letterString;
+    private String tupleString;
+    
+    /**
+     * Instantiates a new rle import presenter.
+     *
+     * @param plugin the procedure plugin reference
+     */
+    public RLEImportPresenter(ProcedurePlugin plugin) {
+        super(plugin);
+    }
 
-	public RLEImportPresenter(ProcedurePlugin plugin) {
-		super(plugin);
-		// TODO Auto-generated constructor stub
-	}
+    @Override
+    public boolean validateEncodeImport() {
+        
+        LetterString content = (LetterString) plugin.getChainHead().getContent();
+        return content.isValid(letterString);
+    }
 
-	@Override
-	public boolean validateEncodeImport() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean validateDecodeImport() {
+        ChainLinkPresenter clp = plugin.getChainHead();
+        
+        while (clp.getNext() != null) {
+            clp = clp.getNext();
+        }   
+        
+        TupleString content = (TupleString) clp.getContent();
+        return content.isValid(tupleString);
+    }
 
-	@Override
-	public boolean validateDecodeImport() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public void handleEncodeImport() {
 
-	@Override
-	public void handleEncodeImport() {
-		// TODO Auto-generated method stub
-		
-	}
+        File file = importFile("Kodieren");
 
-	@Override
-	public void handleDecodeImport() {
-		// TODO Auto-generated method stub
-		
-	}
+        if (file == null) {
+            return;
+        }
+        
+        try {
+            letterString = Files.readString(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
 
-	@Override
-	public AnchorPane getView() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        if (validateEncodeImport()) {
+            procedureLayoutPresenter.switchToChainPresenter();
+        } else {
+            System.err.println("File content not valid.");
+        }
+    }
 
+    @Override
+    public void handleDecodeImport() {
 
+        File file = importFile("Dekodieren");
 
+        if (file == null) {
+            return;
+        }
+        
+        try {
+            tupleString = Files.readString(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        
+        if (validateDecodeImport()) {
+            //TODO notify that we want to decode?
+            procedureLayoutPresenter.switchToChainPresenter();
+        } else {
+            System.err.println("File content not valid.");
+        }
+    }
+
+    @Override
+    public AnchorPane getView() {
+
+        AnchorPane importView = new AnchorPane();
+
+        // loads the template
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("importexample.fxml"));
+            loader.setController(this);
+            importView = loader.load();
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+
+        return importView;
+    }
+
+    /**
+     * Open a FileChooser to import a file.
+     *
+     * @param type the type (i.e. Decode/Encode)
+     * @return the chosen file
+     */
+    private File importFile(String type) {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Datei zum " + type + " auswählen.");
+        return fc.showOpenDialog(null);
+    }
 }
