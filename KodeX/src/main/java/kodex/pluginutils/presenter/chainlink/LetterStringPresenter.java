19fb8e3288d6a8905df4b61c0f1718062304adb7
Leo Kraft <kraftleonhard@gmx.de>
2020-07-20 19:31:56 +0200

    add(RLE ChainLink Presenter)

---
+++ KodeX/src/main/java/kodex/pluginutils/presenter/chainlink/LetterStringPresenter.java
@@ -0,0 +1,32 @@
+package kodex.pluginutils.presenter.chainlink;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import kodex.plugininterface.ChainLinkPresenter;
+import kodex.plugininterface.ChainStep;
+import kodex.pluginutils.model.content.LetterString;
+
+public class LetterStringPresenter extends ChainLinkPresenter {
+
+    public LetterStringPresenter(ChainLinkPresenter previous, ChainStep previousStep, ChainStep nextStep) {
+        super(previous, previousStep, nextStep);
+        this.content = new LetterString();
+    }
+
+    @Override
+    protected void mark(int id) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public AnchorPane getView() {
+        
+        AnchorPane ap = new AnchorPane();
+        Label displaytext = new Label();
+        
+        displaytext.setText(((LetterString)content).getLetterString());
+        ap.getChildren().add(displaytext);
+        return ap;
+    }
+}
