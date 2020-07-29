cd56e7487e5cebcb9dc2402adc5ca85d8c459def
Patrick <uvwut@student.edu>
2020-07-29 20:50:37 +0200

    feat(MatrixToBitList): implemented de & encode

--- KodeX/src/main/java/kodex/pluginutils/model/steps/MatrixToBitList.java
+++ KodeX/src/main/java/kodex/pluginutils/model/steps/MatrixToBitList.java
@@ -2,6 +2,8 @@ package kodex.pluginutils.model.steps;
 
 import kodex.plugininterface.ChainStep;
 import kodex.plugininterface.Content;
+import kodex.pluginutils.model.content.BinaryMatrix;
+import kodex.pluginutils.model.content.BitList;
 import kodex.pluginutils.model.content.RGBList;
 import kodex.pluginutils.model.content.RGBMatrix;
 
@@ -10,8 +12,8 @@ public class MatrixToBitList implements ChainStep {
 
   @Override
   public void decode(Content<?> right, Content<?> left) {
-    RGBMatrix leftmtx = (RGBMatrix) left;
-    RGBList rightlist = (RGBList) right;
+    BinaryMatrix leftmtx = (BinaryMatrix) left;
+    BitList rightlist = (BitList) right;
 
     int width = (int) rightlist.getHeader().get("width");
     int height = (int) rightlist.getHeader().get("height");
@@ -27,8 +29,8 @@ public class MatrixToBitList implements ChainStep {
 
   @Override
   public void encode(Content<?> left, Content<?> right) {
-    RGBMatrix leftmtx = (RGBMatrix) left;
-    RGBList rightlist = (RGBList) right;
+    BinaryMatrix leftmtx = (BinaryMatrix) left;
+    BitList rightlist = (BitList) right;
 
     for (int y = 0; y < leftmtx.getHeight(); y++) {
       for (int x = 0; x < leftmtx.getWidth(); x++) {
