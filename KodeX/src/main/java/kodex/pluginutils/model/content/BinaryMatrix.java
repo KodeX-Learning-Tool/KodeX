d7bb044b7720d260f3373d8d1d6cdc666fd290d3
Patrick <uvwut@student.edu>
2020-07-29 20:36:38 +0200

    feat(Content): implemented BitList & BitMatrix

---
+++ KodeX/src/main/java/kodex/pluginutils/model/content/BinaryMatrix.java
@@ -0,0 +1,36 @@
+package kodex.pluginutils.model.content;
+
+import java.io.File;
+import javafx.scene.paint.Color;
+
+/**
+ * This class holds data in Matrix format. An BinaryMatrix consists of a 2D array [rows][cols]
+ * containing elements of the type Integer.
+ */
+public class BinaryMatrix extends AbstractMatrix<Integer> {
+
+  /**
+   * Creates a new BinaryMatrix with the given dimensions.
+   *
+   * @param width The matrix's width
+   * @param height The matrix's height
+   * @throws IllegalArgumentException If either argument is less than or equal to 0
+   */
+  public BinaryMatrix(int width, int height) throws IllegalArgumentException {
+    if (width <= 0 || height <= 0) {
+      throw new IllegalArgumentException();
+    }
+    super.matrix = new Integer[height][width];
+  }
+
+  @Override
+  protected File toFile() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean isValid(Integer input) {
+    return (input == 0 || input == 1);
+  }
+}
---
+++ KodeX/src/main/java/kodex/pluginutils/model/content/BitList.java
@@ -0,0 +1,28 @@
+package kodex.pluginutils.model.content;
+
+import java.io.File;
+import java.util.LinkedList;
+import javafx.scene.paint.Color;
+
+/**
+ * This class holds data in LinkedList format. An BitList consists of Intergers. Extending
+ * AbstractList, it adds validation and exporting capabilities to Javas List.
+ */
+public class BitList extends AbstractList<Integer> {
+
+  /** Creates a new RGBList. */
+  public BitList() {
+    super.list = new LinkedList<Integer>();
+  }
+
+  @Override
+  protected File toFile() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean isValid(Integer input) {
+    return (input == 0 || input == 1);
+  }
+}
