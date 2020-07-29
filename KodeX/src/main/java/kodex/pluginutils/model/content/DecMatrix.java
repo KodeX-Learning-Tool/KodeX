4d2f0e2caf36d71560830185cc11ba6c2a7f67ed
Patrick <uvwut@student.edu>
2020-07-29 22:02:12 +0200

    feat(DecMatrix): implemented DecMatrix

---
+++ KodeX/src/main/java/kodex/pluginutils/model/content/DecMatrix.java
@@ -0,0 +1,44 @@
+package kodex.pluginutils.model.content;
+
+import java.io.File;
+import javafx.scene.paint.Color;
+
+/**
+ * This class holds data in Matrix format. An DecMatrix consists of a 2D array [rows][cols]
+ * containing elements of the type Color.
+ */
+public class DecMatrix extends AbstractMatrix<Color> {
+
+  /**
+   * Creates a new DecMatrix with the given dimensions.
+   *
+   * @param width The matrix's width
+   * @param height The matrix's height
+   * @throws IllegalArgumentException If either argument is less than or equal to 0
+   */
+  public DecMatrix(int width, int height) throws IllegalArgumentException {
+    if (width <= 0 || height <= 0) {
+      throw new IllegalArgumentException();
+    }
+    super.matrix = new Color[height][width];
+  }
+
+  @Override
+  protected File toFile() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean isValid(Color input) {
+    if (input == null) {
+      return false;
+    }
+    
+    if (input.getRed() != input.getGreen() || input.getGreen() != input.getBlue()) {
+      return false;
+    }
+    
+    return true;
+  }
+}
