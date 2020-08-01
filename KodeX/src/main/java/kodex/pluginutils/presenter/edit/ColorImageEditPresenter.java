ec7cc23c425f124afa19d8a8fa18595e331a537b
uzidy <uzidy@student.kit.edu>
2020-08-01 23:47:10 +0200

    feat(ColorImageEditPresenter): validate input and show alert window if necessary

--- KodeX/src/main/java/kodex/pluginutils/presenter/edit/ColorImageEditPresenter.java
+++ KodeX/src/main/java/kodex/pluginutils/presenter/edit/ColorImageEditPresenter.java
@@ -1,12 +1,16 @@
 package kodex.pluginutils.presenter.edit;
 
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import kodex.model.I18N;
 import kodex.plugininterface.ChainLinkEditPresenter;
 import kodex.plugininterface.ChainLinkPresenter;
 import kodex.pluginutils.model.content.ColorImage;
+import kodex.presenter.PresenterManager;
 
 /**
  * This class manages the edit view and is responsible for editing a color image.
@@ -15,11 +19,19 @@ import kodex.pluginutils.model.content.ColorImage;
  */
 public class ColorImageEditPresenter extends ChainLinkEditPresenter {
   
+  /** The content which this class edits. */
   private ColorImage content;
+  
+  /** The x coordinate of the selected pixel. */
   private int selectedX;
+  
+  /** The y coordinate of the selected pixel. */
   private int selectedY;
+  
+  /** The color picker used for user input. */
   private ColorPicker colorPicker;
   
+  /** The edit view. */
   private AnchorPane view;
   
   /**
@@ -42,7 +54,20 @@ public class ColorImageEditPresenter extends ChainLinkEditPresenter {
 
   @Override
   public void handleSubmit() {
-    content.setColor(selectedX, selectedY, colorPicker.getValue());
+    Color newColor = colorPicker.getValue();
+    
+    // ignores opacity and uses only RGB values
+    if (newColor.getOpacity() != 1) {
+      newColor = new Color(newColor.getRed(), newColor.getGreen(), newColor.getBlue(), 1d);
+      Alert alert = new Alert(AlertType.WARNING);
+      alert.titleProperty().bind(I18N.createStringBinding("alert.title.warning"));
+      alert.headerTextProperty().bind(I18N.createStringBinding("alert.input.invalid"));
+      alert.setContentText("Opacity changes are not supported and won't be saved.");
+      
+      PresenterManager.showAlertDialog(alert);
+    }
+    
+    content.setColor(selectedX, selectedY, newColor);
     chainLinkPresenter.updateChain();
   }
   
