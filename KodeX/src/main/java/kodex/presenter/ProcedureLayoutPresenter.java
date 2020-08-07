f21eb9528d5cd8df626e8af2f9718d11ced8a586
uzidy <uzidy@student.kit.edu>
2020-08-07 19:07:54 +0200

    style(kodex): remove unused imports

--- KodeX/src/main/java/kodex/presenter/ProcedureLayoutPresenter.java
+++ KodeX/src/main/java/kodex/presenter/ProcedureLayoutPresenter.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
