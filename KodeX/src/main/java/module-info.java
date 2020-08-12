3c1b0de2798475f1d57573c151a64b44208ef7c4
Patrick <uvwut@student.edu>
2020-08-12 12:38:00 +0200

    test(SideMenuTypes): wrote tests

--- KodeX/src/main/java/module-info.java
+++ KodeX/src/main/java/module-info.java
@@ -22,6 +22,7 @@ module kodex.KodeX {
   
   opens kodex;
   opens kodex.model;
+  opens kodex.model.validator;
   opens kodex.presenter to javafx.fxml;
   opens kodex.standardplugins.colorimageprocedure.presenter to javafx.fxml;
   opens kodex.standardplugins.rle.presenter to javafx.fxml;
---
+++ KodeX/src/test/java/kodex/model/SideMenuTypesTest.java
@@ -0,0 +1,15 @@
+package kodex.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SideMenuTypesTest {
+
+  @Test
+  void testSideMenuTypes() {
+    assertTrue(SideMenuTypes.EXTENDED_MENU.toString().equals("sidemenu")
+        && SideMenuTypes.MINI_MENU.toString().equals("minisidemenu"));
+  }
+
+}
