a5461057474ffaa0bf68562a99cbb3a72c2c3a4c
uzidy <uzidy@student.kit.edu>
2020-08-12 19:14:29 +0200

    fix(ColorImageImportPresenter): convert RGBa values into RGB values

--- KodeX/src/main/java/kodex/standardplugins/colorimageprocedure/presenter/ColorImageImportPresenter.java
+++ KodeX/src/main/java/kodex/standardplugins/colorimageprocedure/presenter/ColorImageImportPresenter.java
@@ -68,6 +68,12 @@ public class ColorImageImportPresenter extends ImportPresenter {
   
   /** The Constant INVALID_CONTENT_PROPERTY_KEY. */
   private static final String INVALID_CONTENT_PROPERTY_KEY = "alert.content.invalid";
+  
+  /** The Constant MAXIMUM_OPACITY_VALUE as defined in the JavaFX Color class. */
+  private static final double MAXIMUM_OPACITY_VALUE = 1;
+  
+  /** The Constant MINIMUM_OPACITY_VALUE as defined in the JavaFX Color class. */
+  private static final double MINIMUM_OPACITY_VALUE = 0;
 
   /**
    * Instantiates a new color image import presenter.
@@ -157,6 +163,18 @@ public class ColorImageImportPresenter extends ImportPresenter {
         for (int x = 0; x < width; x++) {
           // Retrieving the color of the pixel of the loaded image
           Color color = pixelReader.getColor(x, y);
+          
+          // remove alpha (opacity values) because this procedure uses only RGB values
+          if (color.getOpacity() == MINIMUM_OPACITY_VALUE) {
+            color = Color.WHITE;
+          } else if (color.getOpacity() != MAXIMUM_OPACITY_VALUE) {
+            // adjust the RGB values since the background is white
+            color = new Color(
+                color.getRed() * color.getOpacity() + MAXIMUM_OPACITY_VALUE - color.getOpacity(),
+                color.getGreen() * color.getOpacity() + MAXIMUM_OPACITY_VALUE - color.getOpacity(),
+                color.getBlue() * color.getOpacity() + MAXIMUM_OPACITY_VALUE - color.getOpacity(),
+                MAXIMUM_OPACITY_VALUE);
+          }
 
           // Setting the color to the writable image
           writer.setColor(x, y, color);
