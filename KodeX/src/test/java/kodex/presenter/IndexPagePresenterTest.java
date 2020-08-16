87767620c885c91181f28f2af3a13faaf3b8f965
Leo Kraft <kraftleonhard@gmx.de>
2020-08-16 22:16:38 +0200

    test(IndexPagePresenter): Add test
    Throws exception when beeing executed with the other tests but not when alone.

---
+++ KodeX/src/test/java/kodex/presenter/IndexPagePresenterTest.java
@@ -0,0 +1,61 @@
+package kodex.presenter;
+
+import java.lang.reflect.Field;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.testfx.api.FxAssert;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.LabeledMatchers;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import kodex.model.I18N;
+
+@ExtendWith(ApplicationExtension.class)
+class IndexPagePresenterTest {
+
+  private PresenterManager pm;
+  private PresenterFactory pf;
+  private IndexPagePresenter indexPage;
+
+  @Start
+  void setUp(Stage stage) throws Exception {
+    pm = Mockito.mock(PresenterManager.class);
+    pf = Mockito.mock(PresenterFactory.class);
+
+    indexPage = new IndexPagePresenter(pm, pf);
+
+    Field view = indexPage.getClass().getSuperclass().getDeclaredField("view");
+    view.setAccessible(true);
+
+    BorderPane layout = new BorderPane();
+    layout.setCenter((Node) view.get(indexPage));
+    stage.setScene(new Scene(layout));
+    stage.show();
+  }
+
+  @Test
+  void testHeaderLabel() {
+    FxAssert.verifyThat(
+        ".default-page-title", LabeledMatchers.hasText(I18N.get("indexpage.header")));
+  }
+
+  @Test
+  void testSearchPrompt() {
+    FxAssert.verifyThat(
+        ".network__controll__input__text-field",
+        LabeledMatchers.hasText(I18N.get("indexpage.searchbar.prompt")));
+  }
+
+  @Test
+  void testFilterPrompt() {
+    FxAssert.verifyThat(
+        ".default-choice-box--shadow",
+        LabeledMatchers.hasText(I18N.get("indexpage.filterbox.prompt")));
+  }
+}
