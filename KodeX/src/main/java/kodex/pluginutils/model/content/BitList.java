6fe52f0a76ba44dcfa3d7bcb18b715afd6d81c5d
Yannick Neubert <ulzif@student.kit.edu>
2020-08-22 21:58:24 +0200

    test(content): add export test for list subclasses

--- KodeX/src/main/java/kodex/pluginutils/model/content/BitList.java
+++ KodeX/src/main/java/kodex/pluginutils/model/content/BitList.java
@@ -70,15 +70,17 @@ public class BitList extends AbstractList<Integer> {
 
       //header
       writer.write("HEADER\n");
-      @SuppressWarnings("unchecked")
-      HashMap<String, Object> map = (HashMap<String, Object>) header;
-      map.forEach((key, value) -> { 
-        try {
-          writer.write(key + " " + value + "\n");
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      });
+      if (header != null) {
+        @SuppressWarnings("unchecked")
+        HashMap<String, Object> map = (HashMap<String, Object>) header;
+        map.forEach((key, value) -> { 
+          try {
+            writer.write(key + " " + value + "\n");
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        });
+      }
 
       //content
       writer.write("CONTENT\n");
--- KodeX/src/test/java/kodex/pluginutils/model/content/ByteListTest.java
+++ KodeX/src/test/java/kodex/pluginutils/model/content/ByteListTest.java
@@ -1,12 +1,15 @@
 package kodex.pluginutils.model.content;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
+import java.util.Scanner;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import kodex.exceptions.InvalidInputException;
@@ -16,8 +19,8 @@ class ByteListTest {
   private static ByteList blist;
   private static LinkedList<String> testlist;
   
-  @BeforeAll
-  static void setUpBeforeClass() throws Exception {
+  @BeforeEach
+  void setUp() throws Exception {
     blist = new ByteList();
     testlist = new LinkedList<String>();
     for (int i = 0; i < len; i++) {
@@ -26,13 +29,27 @@ class ByteListTest {
         s += (int) (Math.random() * 2);
       }
       testlist.add(s);
-      System.out.println(testlist.get(i));
     }
   }
 
   @Test
-  void testExport() {
-    fail("Not yet implemented");
+  void testExport() throws FileNotFoundException {
+    blist.setList(testlist);
+    File f = new File("export test");
+    blist.export(f);
+    Scanner scanner = new Scanner(f);
+    assertEquals("HEADER", scanner.nextLine());
+    assertEquals("CONTENT", scanner.nextLine());
+    
+    int y = 0;
+    while (scanner.hasNextLine()) {
+      String in = scanner.nextLine();
+      String is = blist.get(y).substring(0, 8);
+      assertEquals(is, in);
+      y++;
+    }
+    scanner.close();
+    f.delete();
   }
 
   @Test
