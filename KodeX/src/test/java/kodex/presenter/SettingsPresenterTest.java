35da06a967ef32e559e8fea84be3bce34ed99179
Leo Kraft <kraftleonhard@gmx.de>
2020-08-23 21:21:37 +0200

    test(SettingsPresenterTest): Add more tests

--- KodeX/src/test/java/kodex/presenter/SettingsPresenterTest.java
+++ KodeX/src/test/java/kodex/presenter/SettingsPresenterTest.java
@@ -1,12 +1,12 @@
-/** */
 package kodex.presenter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Locale;
+import java.util.Random;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -18,7 +18,10 @@ import org.testfx.framework.junit5.Start;
 import org.testfx.service.finder.NodeFinder;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import kodex.model.DefaultSettings;
@@ -48,11 +51,14 @@ class SettingsPresenterTest {
   }
 
   @AfterAll
-  static void celanUp() {
+  static void cleanUp() {
     DefaultSettings.getInstance().reset();
   }
 
-  /** Test method for {@link kodex.presenter.SettingsPresenter#handleBrowsePath()}. */
+  /**
+   * Test method for {@link kodex.presenter.SettingsPresenter#handleBrowsePath()}. 
+   */
+  @Disabled("Not on FxThread exception is thrown when executing.")
   @Test
   void testHandleBrowsePath() {
 
@@ -79,52 +85,142 @@ class SettingsPresenterTest {
     for (Locale locale : choiceBox.getItems()) {
       robot.clickOn("#languageChoiceBox").clickOn(locale.getDisplayLanguage(locale));
 
-      assertEquals(I18N.getLocale(), locale);
+      assertEquals(locale, I18N.getLocale());
     }
   }
 
-  /** Test method for {@link kodex.presenter.SettingsPresenter#handleChangeLanguage()}. */
-  @Test
-  void testHandleChangeLanguage() {
-    fail("Not yet implemented");
-  }
-
-  /** Test method for {@link kodex.presenter.SettingsPresenter#handleRestoreDefaultSettings()}. */
+  /** 
+   * Test method for {@link kodex.presenter.SettingsPresenter#handleRestoreDefaultSettings()}. 
+   */
+  @Disabled
   @Test
   void testHandleRestoreDefaultSettings() {
-    fail("Not yet implemented");
-  }
-
-  /** Test method for {@link kodex.presenter.SettingsPresenter#handleSubmitPort()}. */
-  @Test
-  void testHandleSubmitPort() {
-    fail("Not yet implemented");
+    // TODO: How can we catch an alert?
   }
 
   /**
-   * Test method for {@link kodex.presenter.Presenter#Presenter(kodex.presenter.PresenterManager,
-   * java.lang.String)}.
+   * Test method for {@link kodex.presenter.SettingsPresenter#handleSubmitPort()}. 
    */
   @Test
-  void testPresenter() {
-    fail("Not yet implemented");
+  void testHandleSubmitPort(FxRobot robot) {
+
+    NodeFinder nodeFinder = FxService.serviceContext().getNodeFinder();
+    TextField portTextField = (TextField) nodeFinder.lookup("#portTextField").query();
+
+    Random r = new Random();
+
+    int randomInt = r.nextInt(65535);
+
+    portTextField.setText(Integer.toString(randomInt));
+
+    robot.clickOn("#submitPortButton");
+
+    assertEquals(randomInt, DefaultSettings.getPort());
+  }
+  
+  @Disabled("TestFx can't handle alerts.")
+  @Test
+  void testPortToHigh(FxRobot robot) {
+    // TODO: How to handle alerts?
+    NodeFinder nodeFinder = FxService.serviceContext().getNodeFinder();
+    TextField portTextField = (TextField) nodeFinder.lookup("#portTextField").query();
+
+    portTextField.clear();
+
+    int port = 65536;
+
+    int previousPort = DefaultSettings.getPort();
+
+    robot.clickOn("#portTextField").write(Integer.toString(port));
+
+    robot.clickOn("#submitPortButton");
+
+    assertEquals(previousPort, DefaultSettings.getPort());
   }
 
-  /** Test method for {@link kodex.presenter.Presenter#getView()}. */
   @Test
-  void testGetView() {
-    fail("Not yet implemented");
+  void testPortToLow(FxRobot robot) {
+    // TODO: How to handle alerts?
+    NodeFinder nodeFinder = FxService.serviceContext().getNodeFinder();
+    TextField portTextField = (TextField) nodeFinder.lookup("#portTextField").query();
+
+    portTextField.clear();
+
+    int port = -1;
+
+    int previousPort = DefaultSettings.getPort();
+
+    robot.clickOn("#portTextField").write(Integer.toString(port));
+
+    robot.clickOn("#submitPortButton");
+
+    // you need to type - and 1 so - is not accepted but 1
+    assertEquals(1, DefaultSettings.getPort());
+  }
+  
+  @Disabled("TestFx can't handle alerts.")
+  @Test
+  void testPortNotANumber(FxRobot robot) {
+    // TODO: How to handle alerts?
+    NodeFinder nodeFinder = FxService.serviceContext().getNodeFinder();
+    TextField portTextField = (TextField) nodeFinder.lookup("#portTextField").query();
+
+    portTextField.clear();
+
+    int previousPort = DefaultSettings.getPort();
+
+    robot.clickOn("#portTextField").write("SomeText");
+
+    robot.clickOn("#submitPortButton");
+
+    assertEquals(previousPort, DefaultSettings.getPort());
   }
 
-  /** Test method for {@link kodex.presenter.Presenter#onExit()}. */
   @Test
-  void testOnExit() {
-    fail("Not yet implemented");
+  void testHeaderLabel() {
+    FxAssert.verifyThat(
+        "#lblHeader", (Label header) -> header.getText().equals(I18N.get("settingspage.header")));
   }
 
-  /** Test method for {@link kodex.presenter.Presenter#loadFXML(java.lang.String)}. */
   @Test
-  void testLoadFXML() {
-    fail("Not yet implemented");
+  void testLanguageHeaderLabel() {
+    FxAssert.verifyThat(
+        "#lblLanguage",
+        (Label header) -> header.getText().equals(I18N.get("settingspage.language.header")));
+  }
+
+  @Test
+  void testDefaultPortHeaderLabel() {
+    FxAssert.verifyThat(
+        "#lblDefaultPort",
+        (Label header) -> header.getText().equals(I18N.get("settingspage.defaultport.header")));
+  }
+
+  @Test
+  void testDefaultPortDescriptionLabel() {
+    FxAssert.verifyThat(
+        "#lblDefaultPortDescription",
+        (Label header) -> header.getText().equals(I18N.get("settingspage.defaultport.lbl")));
+  }
+
+  @Test
+  void testDefaultPathHeaderLabel() {
+    FxAssert.verifyThat(
+        "#lblDefaultPath",
+        (Label header) -> header.getText().equals(I18N.get("settingspage.defaultpath.header")));
+  }
+
+  @Test
+  void testDefaultPathDescriptionLabel() {
+    FxAssert.verifyThat(
+        "#lblDefaultPathDescription",
+        (Label header) -> header.getText().equals(I18N.get("settingspage.defaultpath.lbl")));
+  }
+
+  @Test
+  void testResetButtonLabel() {
+    FxAssert.verifyThat(
+        "#resetButton",
+        (Button button) -> button.getText().equals(I18N.get("settingspage.resetbutton")));
   }
 }
