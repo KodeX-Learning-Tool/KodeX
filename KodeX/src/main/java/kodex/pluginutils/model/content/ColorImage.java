8a2aac21619dbe57ea1d5ddd788e1428682bb88d
Yannick Neubert <ulzif@student.kit.edu>
2020-08-23 23:16:48 +0200

    fix(image plugins): fix exporting of images

--- KodeX/src/main/java/kodex/pluginutils/model/content/ColorImage.java
+++ KodeX/src/main/java/kodex/pluginutils/model/content/ColorImage.java
@@ -1,9 +1,12 @@
 package kodex.pluginutils.model.content;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+
+import javax.imageio.ImageIO;
 
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.WritableImage;
@@ -57,41 +60,21 @@ public class ColorImage extends AbstractImage {
   
   @Override
   public void export(File file) {
+    BufferedImage image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+
+    for (int y = 0; y < getHeight(); y++) {
+      for (int x = 0; x < getWidth(); x++) {
+        String hexColor = getColor(x, y).toString().substring(0, 8);
+        Color c = Color.decode(hexColor);
+        image.setRGB(x, y, c.getRGB());
+      }
+    }
+      
+     
     try {
-      FileWriter writer = new FileWriter(file);
-
-      //header
-      writer.write("HEADER\n");
-      if (header != null) {
-        HashMap<String, Object> map = (HashMap<String, Object>) header;
-        map.forEach((key, value) -> { 
-          try {
-            writer.write(key + " " + value + "\n");
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        });
-      }
-
-      //content
-      writer.write("CONTENT\n");
-      String row = "";
-      for (int y = 0; y < getHeight(); y++) {
-        row = "";
-        for (int x = 0; x < getWidth(); x++) {
-          row += getColor(x, y).toString().substring(0, 8) + " ";
-        }
-        row = row.substring(0, row.length() - 1);
-
-        if (y != getHeight() - 1)  {
-          writer.write(row + "\n");
-        } else {
-          writer.write(row);
-        }
-
-      }
-      writer.close();
+      ImageIO.write(image, "png", file);
     } catch (IOException e) {
+      // TODO Auto-generated catch block
       e.printStackTrace();
     }
 
